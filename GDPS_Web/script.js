05d5b258b2a6f4b16348a6e8f7972557e14f1817
kkwebdev891 <kumari.kiran.652007@gmail.com>
2026-07-05 15:13:54 +0530

    Add files via upload

---
+++ GDPS_Web/script.js
@@ -0,0 +1,21 @@
+// Smooth Scroll
+
+document.querySelectorAll("a").forEach(anchor=>{
+
+anchor.addEventListener("click",function(e){
+
+if(this.getAttribute("href").startsWith("#")){
+
+e.preventDefault();
+
+document.querySelector(this.getAttribute("href")).scrollIntoView({
+
+behavior:"smooth"
+
+});
+
+}
+
+});
+
+});
